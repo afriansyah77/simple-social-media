0c8afaa9f8fbe2d74bf8f3b310c009b9c0e61b26
afriansyah77 <arturafriansyah@gmail.com>
2024-05-02 09:04:07 +0000

    edit workflow

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ composer install
 cp .env.example .env
 php artisan key:generate
 
-# sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.1/g' .env &&
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.1/g' .env &&
 
 php artisan migrate
 php artisan db:seed
